53e72f56fd7181bf07e8d15bdc9f370940de1016
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2025-12-18 16:47:55 -0300

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -2,24 +2,36 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import Products from "./components/Products";
 import Cart from "./components/Cart";
+import RootLayout from "./components/layout";
+import Admin from "./components/Admin";
+import Product from "./components/Product";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/products",
-    element: <Products />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:productId",
+        element: <Product />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+    ],
   },
   {
     path: "/admin",
-    element: <Element />,
+    element: <Admin />,
   },
 ]);
-
 export default router;
